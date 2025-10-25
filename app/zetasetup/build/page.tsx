2e317d6e761d96b2b14904e28ec60c7863492a11
zeta-assistant <zackyogi131@gmail.com>
2025-10-25 17:34:13 +1000

    asdas

---
+++ app/zetasetup/build/page.tsx
@@ -0,0 +1,6 @@
+// app/zetasetup/build/page.tsx (server component; DO NOT import this anywhere)
+import BuildScreen from './BuildScreen';
+
+export default function Page() {
+  return <BuildScreen />;
+}
